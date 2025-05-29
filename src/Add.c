580e1afd2b04f260fa7e09bc6817c8ab14a0f138
GoldenPepperoni <82690051+GoldenPepperoni@users.noreply.github.com>
2025-05-30 00:23:47 +0100

    Fix comment

--- src/Add.c
+++ src/Add.c
@@ -2,21 +2,22 @@
 
 int main()
 {
-    // Initializes variables
+    // Initialise variables
     float A; 
     float B; 
     float C;
 
     printf("=== Calculating A + B ===\n");
     printf("A:");
-    scanf("%f", &A); // Reads an integer from user input and stores it in A.
+    scanf("%f", &A); // Reads a float from user input and stores it in A.
     getchar(); // Waits for user to input. 
     printf("B:");
-    scanf("%f", &B); // Reads an integer from user input and stores it in A.
+    scanf("%f", &B); // Reads a float from user input and stores it in B.
+    getchar(); // Waits for user to input. 
 
-    // Calculates the sum of A and B
+    // Calculate the sum of A and B
     C = A + B; // Store the sum in C
-    printf("A + B = %f\n", C); // Output sum
+    printf("%f + %f = %f\n", A, B, C); // Output sum
     printf("=== End of program ===\n");
     return 0;
 }
